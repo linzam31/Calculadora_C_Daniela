69390a61a4ffdf2d7d05b814630e1139e9096340
linzam31 <linzambrano16@gmail.com>
2026-05-20 15:39:37 -0500

    Eventos click botones

---
+++ JS/funcional.js
@@ -0,0 +1,56 @@
+//Crear las propiedades del objeto
+
+let p = {
+    teclas: document.querySelectorAll("#calculadora ul li"),
+    accion: null,
+    digito: null,
+    operaciones: document.querySelector("#operaciones"),
+    cantisignos: 0,
+    cantdecimal: false,
+    resultado: false
+}
+
+//Crear los métodos del objeto
+
+let m = {
+    inicio : function(){
+        for(let i = 0; i < p.teclas.length; i++){
+            p.teclas[i].addEventListener("click", m.oprimirtecla);
+        }
+       
+    },
+
+    oprimirtecla : function(tecla){
+        p.accion = tecla.target.getAttribute("class");
+        m.calculadora(p.accion);
+
+    },
+
+    calculadora : function(accion){
+
+        switch(accion)
+        {
+
+            case "numero":
+                console.log("numero");
+                break;
+
+                case "simbolo":
+                console.log("simbolo");
+                break;
+
+                case "decimal":
+                console.log("decimal");
+                break;
+
+                case "igual":
+                console.log("igual");
+                break;
+        
+        }
+
+}
+
+}
+
+m.inicio();
